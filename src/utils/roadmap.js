8c31e83a994536977bbaabe74382ab5f4de5bc87
fenggou55688 <jason0971545575@gmail.com>
2025-04-15 19:59:37 +0800

    Update roadmap.js

--- src/utils/roadmap.js
+++ src/utils/roadmap.js
@@ -1,26 +1,11 @@
-// 根據勝負紀錄（如：['莊', '閒', '莊']）產生大路
 export const generateBigRoad = (history) => {
-  const grid = [];
-  let col = 0, row = 0;
-
-  history.forEach((res, i) => {
-    if (i === 0) {
-      grid[0] = [{ value: res }];
-    } else {
-      const prev = history[i - 1];
-      if (res === prev) {
-        if (!grid[col]) grid[col] = [];
-        while (grid[col][row + 1]) row++;
-        grid[col][row + 1] = { value: res };
-        row++;
-      } else {
-        col++;
-        row = 0;
-        if (!grid[col]) grid[col] = [];
-        grid[col][0] = { value: res };
-      }
+  let bigRoad = [];
+  history.forEach((result) => {
+    if (bigRoad.length === 0 || bigRoad[bigRoad.length - 1].length === 6) {
+      bigRoad.push([]);
     }
+    const lastRow = bigRoad[bigRoad.length - 1];
+    lastRow.push(result);
   });
-
-  return grid;
+  return bigRoad;
 };
